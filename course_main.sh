14791415071d4c4db311c2e602a285179bd727ef
EimYokto68Chareontut <eim.y@chareontut.co.th>
2025-03-14 17:46:14 +0700

    asd

--- course_main.sh
+++ course_main.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-
-
-
 #code
 name_select() {
         read -p "Enter name : " name
         echo "Hello : " $name
 }
 count_course(){
- 
         while read -p "Enter count_course: " count && [[ ! "$count" =~ ^[0-9]+$ || "$count" -lt 0 || "$count" -gt 100 ]]; do 
                 echo "Enter 0-100"; 
         done 
-
 }
 
 read_course() {
@@ -25,76 +20,53 @@ read_course() {
         # done
     echo $course;
 }
+
 enter_score() {
-for (( i=1; i<=$count; i++ ))
-do         
-        while read -p "Enter score$i: " grade[$i] && [[ ! "${grade[$i]}" =~ ^[0-9]+$ || "${grade[$i]}" -lt 0 || "${grade[$i]}" -gt 100 ]]; do 
-                echo "Enter 0-100"; 
-        done 
-done
-
-
+        for (( i=1; i<=$count; i++ )) do         
+                while read -p "Enter score$i: " grade[$i] && [[ ! "${grade[$i]}" =~ ^[0-9]+$ || "${grade[$i]}" -lt 0 || "${grade[$i]}" -gt 100 ]]; do 
+                        echo "Enter 0-100"; 
+                done 
+        done
 }
-calculator_a(){
-        for (( i=0; i<=4; i++ )) do 
-                x=$((i+1));
-                if [[ ${grade[$i]} -ge 80 ]]
-                then
-                        echo "score $x =A"
-                        credit[$i]=4
-                elif [[ ${grade[$i]} -ge 70 ]]
-                then
-                        echo "score $x =B"
-                        credit[$i]=3
-                elif [[ ${grade[$i]} -ge 60 ]]
-                then
-                        echo "score $x =C"
-                        credit[$i]=2
-                elif [ ${grade[$i]} -ge 50 ]
-                then
-                        credit[$i]=1
-                        echo "score $x =D"
-                else
-                        credit[$i]=0
-                        echo "score $x =F"
-                fi
-        done
-        sum=$((credit[0]+credit[1]+credit[2]+credit[3]+credit[4]))
-        echo 'GPA: '$(awk "BEGIN {printf \"%.2f\", $sum / ${#credit[@]} }")
-        }
-calculator_b(){
-        for (( i=0; i<=4; i++ )) do 
-                x=$((i+1));
-                if [[ ${grade[$i]} -ge 95 ]]
-                then
-                        echo "score $x =A"
-                        credit[$i]=4
-                elif [[ ${grade[$i]} -ge 80 ]]
-                then
-                        echo "score $x =B"
-                        credit[$i]=3
-                elif [[ ${grade[$i]} -ge 75 ]]
-                then
-                        echo "score $x =C"
-                        credit[$i]=2
-                elif [ ${grade[$i]} -ge 0 ]
-                then
-                        credit[$i]=1
-                        echo "score $x =D"
-                fi
-        done
-        sum=$((credit[0]+credit[1]+credit[2]+credit[3]+credit[4]))
-        echo 'GPA: '$(awk "BEGIN {printf \"%.2f\", $sum / ${#credit[@]} }")
-        }
 
+pull_course(){
+        cat ./course/course_$course.txt|while read iline ;do
+        x=$((x+1));
+        echo $iline | awk '{print $1}'
+        echo '---------------------'
+        pullcourse_low[$x]=$(echo $iline | awk '{print $1}')
+        echo ${pullcourse_low[$x]}
+        echo '---------------------'
+        # pullcourse_high[$x]=$(echo $iline | cut -c6-8)
+        #     echo $iline | cut -c1-2
+        # echo ${pullcourse_low[1]}
+        # echo ${pullcourse_high[1]}
+        #echo '---------------------'
+        done
+}
 
+# exit
+for (( i=1; i<=$count; i++ )) do 
+       echo $i
+       if [[ ${grade[$i]} -ge ${pullcourse_low[$i]} ]]; then
+               echo 'A'
+       elif [[ ${grade[$i]} -ge ${pullcourse_low[$i]} ]]; then
+               echo 'B'
+       fi
+       echo ${grade[$i]}
+        
+done
 #Start program
+x=0
 name_select
 count_course
 read_course
+pull_course
 enter_score
-exit
-cat ./course/course_$course.txt
+
+
+
+
 
 
 
--- script.bash
+++ script.bash
@@ -1,9 +1,29 @@
-#!/bin/bash
+# #!/bin/bash
 
 
-cat ./course/course_b.txt|while read iline ;do
-    p=$iline | cut -c1-2
+# cat ./course/course_b.txt|while read iline ;do
+#     # echo $iline | cut -c1-2
+#     po=$(echo $iline | cut -c1-2)
+#     echo $po
+#     echo '---------------------'
+#     done
 
-    echo '---------------------'
-    done
-    
+# cat ./course/course_b.txt|while read iline ;do
+#         x=$x+1 
+#         po[$x]=$(echo $iline | cut -c6-8)
+#         echo ${po[$x]}
+# echo '---------------------'
+
+awk -F "," '{print $1}' ./course/course_b.txt |head
+
+
+#         echo ${po[1]}
+# echo ${po[2]}
+# echo ${po[3]}
+# echo ${po[4]}
+# done
+# echo '|||||||||||||'
+#         echo ${po[1]}
+# echo ${po[2]}
+# echo ${po[3]}
+# echo ${po[4]}
